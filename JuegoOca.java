36fa16cd04246f6699fbeaa9dc6ac5e5829f00a3
JorgeGarciawp10 <134055568+JorgeGarciawp10@users.noreply.github.com>
2023-06-06 18:56:12 +0200

    Update JuegoOca.java
    Añadidas las funciones: aplicarReglasCasilla, obtenerSiguienteOca,
    obtenerJugadorSalvador, activarCasillaExtra,verificarFinJuego, obtenerIndiceJugador,obtenerNombreJugador

--- JuegoOca.java
+++ JuegoOca.java
@@ -108,3 +108,139 @@ public class JuegoOca {
             System.out.println("La casilla actual es nula. Verifica la configuración del tablero.");
         }
     }
+    private void aplicarReglasCasilla(String nombreJugador, TipoCasilla tipoCasilla) {
+        switch (tipoCasilla) {
+            case OCA:
+                System.out.println("El jugador " + nombreJugador + " ha caído en una casilla de Oca. Avanza a la siguiente Oca.");
+                scanner.nextLine();
+                int siguienteOca = obtenerSiguienteOca(posiciones.get(nombreJugador));
+                int numeroCasillas = siguienteOca - posiciones.get(nombreJugador);
+                moverFicha(nombreJugador, numeroCasillas);
+                break;
+            case PUENTE:
+                System.out.println("El jugador " + nombreJugador + " ha caído en una casilla de Puente. Avanza hasta la casilla 12.");
+                scanner.nextLine();
+                moverFicha(nombreJugador, 12 - posiciones.get(nombreJugador));
+                break;
+            case DADOS_TRAVIESOS:
+                System.out.println("El jugador " + nombreJugador + " ha caído en una casilla de Dados Traviesos. Juega de nuevo.");
+                scanner.nextLine();
+                turnoExtra[obtenerIndiceJugador(nombreJugador)] = true;
+                break;
+            case HOTEL:
+                System.out.println("El jugador " + nombreJugador + " ha caído en una casilla de Hotel. Avanzará una casilla extra durante los tres siguientes turnos.");
+                turnoExtra[obtenerIndiceJugador(nombreJugador)] = true;
+                scanner.nextLine();
+                activarCasillaExtra(nombreJugador, 3);
+                break;
+            case CARCEL:
+                System.out.println("El jugador " + nombreJugador + " ha caído en una casilla de Cárcel. Pierde un turno.");
+                scanner.nextLine();
+                break;
+            case COCHE:
+                System.out.println("El jugador " + nombreJugador + " ha caído en una casilla de Coche. Avanza hasta la casilla 19.");
+                scanner.nextLine();
+                moverFicha(nombreJugador, 19 - posiciones.get(nombreJugador));
+                break;
+            case UNICORNIO:
+                System.out.println("El jugador " + nombreJugador + " ha caído en una casilla de Unicornio. Lanzará dos dados en sus próximos dos turnos.");
+                scanner.nextLine();
+                dobleDado[obtenerIndiceJugador(nombreJugador)] = true;
+                activarCasillaExtra(nombreJugador, 2);
+                break;
+            case MUERTE:
+                System.out.println("El jugador " + nombreJugador + " ha caído en una casilla de Muerte. Vuelve a la casilla 1.");
+                scanner.nextLine();
+                moverFicha(nombreJugador, 1 - posiciones.get(nombreJugador));
+                break;
+            case LABERINTO:
+                System.out.println("El jugador " + nombreJugador + " ha caído en una casilla de Laberinto. Otro jugador puede salvarlo lanzando un número mayor que 4.");
+                scanner.nextLine();
+                String jugadorSalvador = obtenerJugadorSalvador(nombreJugador);
+                if (!jugadorSalvador.isEmpty()) {
+                    System.out.println("El jugador " + jugadorSalvador + " ha lanzado un número mayor que 4. El jugador " + nombreJugador + " es salvado y avanza una casilla extra.");
+                    jugadorSalvado[obtenerIndiceJugador(nombreJugador)] = true;
+                    moverFicha(nombreJugador, 1);
+                } else {
+                    System.out.println("Ningún jugador puede salvar al jugador " + nombreJugador + ". Deberá esperar al próximo turno para intentarlo de nuevo.");
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private int obtenerSiguienteOca(int posicion) {
+        int siguienteOca = -1;
+        for (int i = posicion + 1; i < Tablero.NUM_CASILLAS; i++) {
+            if (tablero.obtenerCasilla(i).getTipo() == TipoCasilla.OCA) {
+                siguienteOca = i;
+                break;
+            }
+        }
+        return siguienteOca;
+    }
+
+    private String obtenerJugadorSalvador(String jugador) {
+        String jugadorSalvador = "";
+        for (Map.Entry<String, Integer> entry : posiciones.entrySet()) {
+            String nombreJugador = entry.getKey();
+            int posicion = entry.getValue();
+
+            if (!nombreJugador.equals(jugador) && !jugadorSalvado[obtenerIndiceJugador(nombreJugador)]) {
+                int numeroDado = random.nextInt(6) + 1;
+                if (numeroDado > 4) {
+                    jugadorSalvador = nombreJugador;
+                    break;
+                }
+            }
+        }
+        return jugadorSalvador;
+    }
+
+    public void activarCasillaExtra(String nombreJugador, int numTurnos) {
+        // Verificar si el jugador está en una casilla de hotel
+        if (tablero.obtenerCasilla(posiciones.get(nombreJugador)).getTipo() == TipoCasilla.HOTEL) {
+            turnoExtra[obtenerIndiceJugador(nombreJugador)] = true;
+        }
+    }
+
+    private boolean verificarFinJuego(String nombreJugador) {
+        if (posiciones.get(nombreJugador) == Tablero.NUM_CASILLAS - 1) {
+            System.out.println("\n¡El jugador " + nombreJugador + " ha llegado a la casilla final!");
+            System.out.println("¡Felicidades! ¡Has ganado el juego de la Oca!");
+            return true;
+        }
+        return false;
+    }
+    
+    private int obtenerIndiceJugador(String nombreJugador) {
+        int indice = -1;
+        int i = 0;
+        for (Map.Entry<String, Integer> entry : posiciones.entrySet()) {
+            if (entry.getKey().equals(nombreJugador)) {
+                indice = i;
+                break;
+            }
+            i++;
+        }
+        return indice;
+    }
+
+    
+    private String obtenerNombreJugador(int indice) {
+        // Obtener el nombre del jugador correspondiente al índice
+        String nombre = null;
+        int i = 0;
+        for (Map.Entry<String, Integer> entry : posiciones.entrySet()) {
+            if (i == indice) {
+                nombre = entry.getKey();
+                break;
+            }
+            i++;
+        }
+        return nombre;
+    }
+
+    
+}
